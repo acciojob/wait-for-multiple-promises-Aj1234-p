3e634573b2c620592e74015979dcec215caff321
Ajit Pandey <ajitytsurat0707@gmail.com>
2026-08-29 14:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-const output = document.querySelector('#output');
-
-const totalTime1 = new Date().getSeconds().toFixed(2);
+let output = document.querySelector('#output');
 const delayTime1 = 1000, delayTime2 = 2000, delayTime3 = 3000;
 const Promise1 = ()=>{
   return new Promise((resolve,reject)=>{
     setTimeout(()=>{
-      let trow = document.createElement('tr');
-      let tdata1 = document.createElement('td');
-      let tdata2 = document.createElement('td');
-      tdata1.textContent = 'Promise 2';
-      tdata2.textContent = 2;
-      trow.appendChild(tdata1);
-      trow.appendChild(tdata2);
-      output.appendChild(trow);
-      console.log("this is first promise")
       resolve(delayTime2);
     },delayTime2);
   })
@@ -21,42 +10,34 @@ const Promise1 = ()=>{
 const Promise2 = ()=>{
   return new Promise((resolve,reject)=>{
     setTimeout(()=>{
-      output.firstElementChild.remove();
-      let trow = document.createElement('tr');
-      let tdata1 = document.createElement('td');
-      let tdata2 = document.createElement('td');
-      tdata1.textContent = 'Promise 1';
-      tdata2.textContent = 1;
-      trow.appendChild(tdata1);
-      trow.appendChild(tdata2);
-      output.appendChild(trow);
-      console.log("this is second promise")
-      resolve(delayTime1)
+      resolve(delayTime1);
     },delayTime1);
   })
 }
 const Promise3 = ()=>{
   return new Promise((resolve,reject)=>{
     setTimeout(()=>{
-      let trow = document.createElement('tr');
-      let tdata1 = document.createElement('td');
-      let tdata2 = document.createElement('td');
-      tdata1.textContent = 'Promise 3';
-    tdata2.textContent = 3;
-      trow.appendChild(tdata1);
-      trow.appendChild(tdata2);
-      output.appendChild(trow);
-      console.log("this is third promise")
-      resolve(delayTime3)
+      resolve(delayTime3);
     },delayTime3);
   })
 }
 
 let start = new Date().getSeconds().toFixed(2);
 Promise.all([Promise1(),Promise2(),Promise3()]).then((data)=>{
-  console.log(data, new Date())
-  let end = new Date().getSeconds().toFixed(2);
-  let totalSeconds = end-start;
+  console.log(data);
+  output.firstElementChild.remove();
+   let end = new Date().getSeconds().toFixed(2);
+   let totalSeconds = end-start;
+  for(let i=0;i<data.length;i++){
+    let trow = document.createElement('tr');
+    let tdata1 = document.createElement('td');
+    let tdata2 = document.createElement('td');
+    tdata1.textContent = `Promise ${i+1}`
+    tdata2.textContent = data[i]/1000;
+    trow.appendChild(tdata1);
+    trow.appendChild(tdata2);
+    output.appendChild(trow);
+  }
   let trow = document.createElement('tr');
   let tdata1 = document.createElement('td');
   let tdata2 = document.createElement('td');
@@ -67,3 +48,4 @@ Promise.all([Promise1(),Promise2(),Promise3()]).then((data)=>{
   output.appendChild(trow);
 
 })
+
